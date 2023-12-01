65c995ece92ab137e36269aaf791157f846b5e01
Rameshwarrr <146844875+Rameshwarrr@users.noreply.github.com>
2023-12-01 11:30:16 +0530

    Add files via upload

---
+++ nestedif.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enter your number");
+    scanf("%d%d%d", &a,&b,&c);
+    
+    
+        if(a>c)
+        {
+            printf("a is grater number=%d",a);
+        
+        else
+        
+            printf("c is greater number=%d",c);
+        }
+    
+    else
+    {
+        if(b>c)
+           printf("b is greatwe number=%d",b);
+        else
+            printf("c is greatest number=%d",c);
+    }
+
+
+    
+}
+    
